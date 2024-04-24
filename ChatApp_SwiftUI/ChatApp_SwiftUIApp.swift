09f797396920b97aaec65fed1cfdb4f608db3363
jooeun kim <jay311311@gmail.com>
2024-04-23 23:03:36 -0400

    Initial Commit

---
+++ ChatApp_SwiftUI/ChatApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp_SwiftUIApp.swift
+//  ChatApp_SwiftUI
+//
+//  Created by Jooeun Kim on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatApp_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatApp_SwiftUI
+//
+//  Created by Jooeun Kim on 2024-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
